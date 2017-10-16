14289af75a1aff0a25d2bbbc5306cf615461e63b
Zaur Giyasov <zaurusnnov@icloud.com>
2017-10-16 07:17:06 +0300

    [Assignment 4, Smashtag] Add Mentions View (not configured)

---
+++ Smashtag/ImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageTableViewCell.swift
+//  Smashtag
+//
+//  Created by ZaurNNov on 16.10.2017.
+//  Copyright © 2017 ZaurNNov. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
