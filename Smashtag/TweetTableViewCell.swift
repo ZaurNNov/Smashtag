4cf4b46b79ac2df64e54352d539693ec4907f76a
Zaur Giyasov <zaurusnnov@icloud.com>
2017-10-16 06:14:50 +0300

    [Assignment 4, Smash-tag] Add color for some labels

--- Smashtag/TweetTableViewCell.swift
+++ Smashtag/TweetTableViewCell.swift
@@ -12,19 +12,37 @@ import Twitter
 class TweetTableViewCell: UITableViewCell {
 
     @IBOutlet weak var tweetProfileImageView: UIImageView!
-    
     @IBOutlet weak var tweetCreatedLabel: UILabel!
-    
     @IBOutlet weak var tweetUserLabel: UILabel!
-    
     @IBOutlet weak var tweetTextLabel: UILabel!
-    
     
     var tweet: Twitter.Tweet? {didSet {updateUI()}}
     
+    struct differentColorMentiones {
+        static let hashtagNowPurple = UIColor.purple
+        static let urlNowBlue = UIColor.blue
+        static let userNamesNowOrange = UIColor.orange
+    }
+    
+    private func setTextLabel(_ tweet: Twitter.Tweet?) -> NSMutableAttributedString {
+        guard let tweet = tweet else {return NSMutableAttributedString(string: "")}
+        var tweetText: String = tweet.text
+        
+        for _ in tweet.media {tweetText += " 📷"}
+        let attributetText = NSMutableAttributedString(string: tweetText)
+        
+        attributetText.setMentionsColor(tweet.hashtags, color: differentColorMentiones.hashtagNowPurple)
+        attributetText.setMentionsColor(tweet.urls, color: differentColorMentiones.urlNowBlue)
+        attributetText.setMentionsColor(tweet.userMentions, color: differentColorMentiones.urlNowBlue)
+        
+        return attributetText
+    }
+    
     private func updateUI() {
-        tweetTextLabel?.text = tweet?.text
         tweetUserLabel?.text = tweet?.user.description
+        
+        //add color attributes
+        tweetTextLabel?.attributedText = setTextLabel(tweet)
         
         if let profileImageURL = tweet?.user.profileImageURL {
             //Fix this main thread
@@ -49,4 +67,12 @@ class TweetTableViewCell: UITableViewCell {
         }
     }
     
+}
+
+private extension NSMutableAttributedString {
+    func setMentionsColor(_ mention: [Mention], color: UIColor) {
+        for mention in mention {
+            addAttribute(NSAttributedStringKey.foregroundColor, value: color, range: mention.nsrange)
+        }
+    }
 }
