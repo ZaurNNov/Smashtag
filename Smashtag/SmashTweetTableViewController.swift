93dd53242400a5019e43da106c9fef841edfcece
Zaur Giyasov <zagiyas@mera.ru>
2017-10-17 15:06:20 +0300

    CoreData working! fix: thread (load/read database in main thread)

--- Smashtag/SmashTweetTableViewController.swift
+++ Smashtag/SmashTweetTableViewController.swift
@@ -12,6 +12,9 @@ import CoreData
 
 class SmashTweetTableViewController: TweetTableViewController {
 
+    //coreData container
+    var container: NSPersistentContainer? = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+    
     override func insertTweets(_ newTweets: [Twitter.Tweet]){
         super.insertTweets(newTweets)
         updateDatabase(with: newTweets)
@@ -19,6 +22,38 @@ class SmashTweetTableViewController: TweetTableViewController {
     }
     
     private func updateDatabase(with tweets: [Twitter.Tweet]) {
+        print("Starting database load")
+        container?.performBackgroundTask{ [weak self] context in
+            for tweeterInfo in tweets {
+                //add tweet
+                _ = try? Tweet.findOrCreateTweet(matching: tweeterInfo, in: context)
+                
+            }
+            try? context.save()
+            print("done loading database")
+            self?.printsDatabaseStatistics()
+        }
         
+    }
+    
+    private func printsDatabaseStatistics(){
+        if let context = container?.viewContext {
+            context.perform {
+                if Thread.isMainThread {
+                    print("is Main Thread!")
+                } else {
+                    print("is Not Main Thread!")
+                }
+                
+                let request: NSFetchRequest<Tweet> = Tweet.fetchRequest()
+                if let tweetCount = (try? context.fetch(request))?.count {
+                    print("\(tweetCount) tweets")
+                }
+                
+                if let tweeterCount = try? context.count(for: TwitterUser.fetchRequest()) {
+                    print("\(tweeterCount) Twitter users")
+                }
+            }
+        }
     }
 }
